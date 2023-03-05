6f9bda08fcc951443eb316fccfe91c293e831ee5
Matthew Macon <matthewmacon90@gmail.com>
2023-03-05 17:16:54 -0500

    updates

---
+++ foundations/javascript/notes.js
@@ -0,0 +1,26 @@
+// Notes:
+
+/* EMMET:
+    li*5 makes 5 li elements in html.
+
+Extentions:
+Material Theme
+CSS Peek
+Color highlight
+Bracket Pair Colorizer
+use control comma to get to settings for the editor
+*/
+
+
+// const hello = () => {
+//     if(dosomething) {
+
+//     }
+// }
+
+
+// let idk = 'I have no idea what I\'m doing';
+// console.log(idk[13]);
+// idk = idk.slice(0, 13); // 'I have no idea';
+// console.log(idk);
+
